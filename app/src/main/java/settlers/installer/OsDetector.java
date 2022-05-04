1ae3b6ea83cb66f43a1dd87fd4a6d10f0b468922
hiran <hiran@homeserver.indernet>
2022-05-04 21:41:53 +0200

    add javadoc

--- app/src/main/java/settlers/installer/OsDetector.java
+++ app/src/main/java/settlers/installer/OsDetector.java
@@ -3,7 +3,8 @@
 package settlers.installer;
 
 /**
- *
+ * Detects the Operating System this application is running on.
+ * 
  * @author hiran
  */
 public class OsDetector {
@@ -14,6 +15,11 @@ public class OsDetector {
     public static final boolean IS_UNIX = (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
     public static final boolean IS_SOLARIS = (OS.indexOf("sunos") >= 0);
 
+    /**
+     * 'Test' method
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
 
         System.out.println("os.name: " + OS);
